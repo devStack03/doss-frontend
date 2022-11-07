db46f8739fded7c7f111e38bbc654ec448c0eb42
devStack03 <devking0325@gmail.com>
2022-11-07 15:03:01 +0200

    update: signup page design

--- src/pages/PaymentSuccess.tsx
+++ src/pages/PaymentSuccess.tsx
@@ -1,5 +1,6 @@
 import NavHeader from '../components/NavHeader';
-import PaymentSuccessCheckMark from '../assets/images/check-circle.png';
+import PaymentSuccessCheckMark from '../assets/svgs/success-checkmark.svg';
+import PaymentSuccessBtn from '../assets/svgs/success-btn.svg'
 
 const PaymentSuccess = () => {
   return (
@@ -7,13 +8,21 @@ const PaymentSuccess = () => {
       <NavHeader />
       <div className="section-fullscreen wf-section  tw-flex tw-justify-center tw-items-center">
         <div className="section-fullscreen-container">
-          <div className="telephone-form tw-max-w-lg">
-            <div className="fullscreen--title">Hemos recibido tu pago.</div>
-            <div className="fullscreen--subtitle tw-mt-7">Tu suscripcion ha sido procesada correctamente
-              Pronto recibiras un correcto electronico de confirmacion.
+          <div className="telephone-form tw-w-[570px] tw-max-w-xl">
+            <div className="fullscreen--title">
+              Hemos recibido tu pago ❤️
+            </div>
+            <div className="fullscreen--subtitle tw-mt-7 tw-mb-12">
+              Pronto recibiras un correo electrónico de confirmación 🎉
             </div>
             <div className="tw-flex tw-justify-center">
               <img src={PaymentSuccessCheckMark} alt="payment-success" />
+            </div>
+
+            <div className="tw-flex tw-justify-center tw-mt-14">
+              <button>
+                <img src={PaymentSuccessBtn} alt="payment-success" />
+              </button>
             </div>
           </div>
         </div>
